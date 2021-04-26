24a55b9a496029b71dc2aac75b06b1910b0ee763
David Erik Nazareno <daviderikfalsh@yahoo.com.br>
2021-04-26 15:35:37 -0300

    aula styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello Word</h1>
+      <h1 className ="title" >Hello Word</h1>
     </div>
   );
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    :root    
+
+* { 
+        margin: 0px,
+        padding: 0px,
+        box-sizing: border box,
+   }
+   body {
+       background: 
+   }
+`
